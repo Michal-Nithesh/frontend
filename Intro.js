a49a14abe5bc99cf282797b5ea777e5e6c7e1437
Michal Nithesh <michalnithesh.cs22@stellamaryscoe.edu.in>
2024-04-04 23:34:26 +0530

    first commit

---
+++ Intro.js
@@ -0,0 +1,15 @@
+let js = {
+    name : "JavaScript",
+    abbreviation : "JS",
+    isAwesome : true,
+    officialSite : "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    officialSpec : "ECMAScript",
+    birthYear : 1995,
+    creator : "Brendan Eich",
+    isDynamic : true,
+}
+
+if (js.isAwesome) {
+    console.log(`${js.name} is an awesome language!`);
+    //shout("Hell yes JS is awesome!");
+}
